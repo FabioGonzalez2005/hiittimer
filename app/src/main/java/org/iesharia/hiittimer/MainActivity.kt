ce95f0d894b2e7c748a0c977330d6f331f866235
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-10-22 12:57:26 +0100

    Creamos la variable de mediaPlayer

--- app/src/main/java/org/iesharia/hiittimer/MainActivity.kt
+++ app/src/main/java/org/iesharia/hiittimer/MainActivity.kt
@@ -17,6 +17,8 @@ import android.media.MediaPlayer
 import org.iesharia.hiittimer.ui.theme.HiittimerTheme
 
 class MainActivity : ComponentActivity() {
+    private var mediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -28,6 +30,10 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer?.release()
     }
 }
 
